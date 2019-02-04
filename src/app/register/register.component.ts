4b4cbf3297dc604e345011f96a28ba9cb4361985
Nare Sai Chaitanya <nare.saichaitanya@gmail.com>
2019-02-04 09:12:12 +0530

    added register component & service file

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,8 +9,12 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   isDoctor: Boolean = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+
+  docPersonalFormGroup: FormGroup;
+  docContactFormGroup: FormGroup;
+  docWorkFormGroup: FormGroup;
+  userFormGroup: FormGroup;
+
   gender = [];
 
   constructor(private _formBuilder: FormBuilder) {
@@ -18,15 +22,51 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.gender = ['Male', 'Female'];
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+
+
+    this.userFormGroup = this._formBuilder.group({
+      userName: ['', Validators.required],
+      userEmail: ['', Validators.required],
+      userPassword: ['', Validators.required],
+      userMobileNumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      userGender: ['', Validators.required],
+      // userDob: ['', Validators.required],
+      userCity: ['', Validators.required],
+      userPincode: ['', Validators.required],
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+    this.docPersonalFormGroup = this._formBuilder.group({
+      docName: ['', Validators.required],
+      docEmail: ['', Validators.required],
+      docPassword: ['', Validators.required],
+      docGender: ['', Validators.required],
+    });
+
+    this.docContactFormGroup = this._formBuilder.group({
+      docMobileNumber: ['', Validators.required],
+      docState: ['', Validators.required],
+      docCity: ['', Validators.required]
+    });
+
+    this.docWorkFormGroup = this._formBuilder.group({
+      docWorksAt: ['', Validators.required],
+      docLicenseNumber: ['', Validators.required],
+      docQualification: ['', Validators.required],
+      docSpecialization: ['', Validators.required],
+      docFee: ['', Validators.required]
     });
   }
 
   checkDoctorOrNot() {
     return this.isDoctor = (this.isDoctor === true) ? false : true;
+  }
+
+  getUserRegisterDetails() {
+    console.log('usr details ', this.userFormGroup.value);
+  }
+
+  getDocRegisterDetails() {
+    console.log('doc personal details ', this.docPersonalFormGroup.value);
+    console.log('doc contact details ', this.docContactFormGroup.value);
+    console.log('doc work details ', this.docWorkFormGroup.value);
   }
 }
